f40bb2c82c1e0bffd49bf85c93f5afbd07d63fe9
CareTaker <caretaker023@gmail.com>
2019-01-31 11:02:41 +0530

    login

--- app/src/main/java/com/example/harshithchoudhary/caretaker/HomeActivity.java
+++ app/src/main/java/com/example/harshithchoudhary/caretaker/HomeActivity.java
@@ -1,39 +1,84 @@
 package com.example.harshithchoudhary.caretaker;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
     private EditText emailmob , password;
     private Button login,servicepro;
     private TextView signup;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        setupUIView();
+
+        emailmob = (EditText) findViewById(R.id.ETmail);
+        password = (EditText) findViewById(R.id.ETpass);
+        login = (Button) findViewById(R.id.BTlogin);
+        servicepro = (Button) findViewById(R.id.BTservicepro);
+        signup = (TextView) findViewById(R.id.TVsignup);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if(user != null){
+            finish();
+            startActivity(new Intent(HomeActivity.this, SigninActivity.class ));
+        }
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(emailmob.getText().toString(),password.getText().toString());
+            }
+        });
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View w) {
+            public void onClick(View v) {
                 startActivity(new Intent(HomeActivity.this,RegistrationActivity.class));
                 finish();
             }
         });
     }
 
-private void setupUIView() {
-    emailmob = (EditText) findViewById(R.id.ETmail);
-    password = (EditText) findViewById(R.id.ETpass);
-    login = (Button) findViewById(R.id.BTlogin);
-    servicepro = (Button) findViewById(R.id.BTservicepro);
-    signup = (TextView) findViewById(R.id.TVsignup);
-}
+    private void validate(String userName,String Password){
+
+        progressDialog.setMessage("SIT Tight Until VERIFIED!");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(userName,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+         //           progressDialog.dismiss();
+                    Toast.makeText(HomeActivity.this,"Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(HomeActivity.this, SigninActivity.class ));
+                 }else
+                    Toast.makeText(HomeActivity.this,"Login Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/harshithchoudhary/caretaker/RegistrationActivity.java
+++ app/src/main/java/com/example/harshithchoudhary/caretaker/RegistrationActivity.java
@@ -1,6 +1,7 @@
 package com.example.harshithchoudhary.caretaker;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +10,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegistrationActivity extends AppCompatActivity {
 
     private EditText name,email,password,mobileno,age,gender;
     private Button signup;
     private TextView backtosignin;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +28,28 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
         setupUIViews();
 
+        firebaseAuth = firebaseAuth.getInstance();
+
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(validate()){
                     //upload data to the database
+
+                    String emailuser = email.getText().toString().trim();
+                    String passuser = password.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(emailuser,passuser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegistrationActivity.this, "Registration Successful",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegistrationActivity.this,HomeActivity.class));
+                            }else{
+                                Toast.makeText(RegistrationActivity.this, "Registration Failed",Toast.LENGTH_SHORT).show();
+                            }
+                            }
+                    });
                 }
             }
         });
@@ -57,12 +81,11 @@ public class RegistrationActivity extends AppCompatActivity {
         String mobileuser = mobileno.getText().toString();
         String genderuser = gender.getText().toString();
 
-        if(nameuser.isEmpty() && emailuser.isEmpty() && passworduser.isEmpty() && mobileuser.isEmpty() && genderuser.isEmpty()){
+        if(nameuser.isEmpty() || emailuser.isEmpty() || passworduser.isEmpty() || mobileuser.isEmpty() || genderuser.isEmpty()){
             Toast.makeText(this, "please enter all the details", Toast.LENGTH_SHORT).show();
         }else{
             result = true;
         }
-
         return result;
     }
 }
